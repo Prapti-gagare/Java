f9997046f65c355802c0065e89ed854cd0eb415b
Prapti-gagare <praptigagare11@gmail.com>
2025-09-27 14:18:01 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,18 @@
+public class VariableDeclaration
+ {
+    public static void main(String[] args) 
+    {
+        int number = 10;               
+        double price = 99.99;         
+        char grade = 'A';           
+        boolean isJavaFun = true;     
+        String name = "Alice";       
+
+        
+        System.out.println("Number: " + number);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Name: " + name);
+    }
+}
