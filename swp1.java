3e5a22253c4d924866c1c5c1d91a4af4b688ae1f
Prapti-gagare <praptigagare11@gmail.com>
2026-01-06 08:53:59 +0530

    Java basic program

---
+++ swp1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class swp1
+{
+    public static void main(String args[])
+    {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        b = sc.nextInt();
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
