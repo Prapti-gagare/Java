24ff6f1f3d3a02590797d68e6aeb33624b5dc02e
Prapti-gagare <praptigagare11@gmail.com>
2026-01-06 09:12:48 +0530

    Java basic program

---
+++ swap2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class swap2
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a, b, temp;
+        System.out.print("Enter first number: ");
+        a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        b = sc.nextInt();
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
